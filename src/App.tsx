d249d1293f4710383a74b1da430921a39bdc214c
Wander Alves <wsilva_94@outlook.com>
2022-10-04 19:11:27 -0300

    feat: criando estrutura básica do sistema e adicionando os primeiros componentes

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Button from "./components/buttons/Button";
+import React from "react";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Hello Word</h1>
+            <Button />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/buttons/Button.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Button extends React.Component {
+    render(): React.ReactNode {
+        return <button>Click</button>;
+    }
+}
+
+export default Button;
